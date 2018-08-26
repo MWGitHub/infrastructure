c9f8f84a811e89f488e43020cad76171257a1d96
Michael Wang <mwwpublic@gmail.com>
2018-08-26 19:37:28 -0400

    refactor: move terraform block into main

--- terraform/backend.tf
+++
@@ -1,6 +0,0 @@
-terraform {
-  backend "gcs" {
-    prefix = "terraform/state"
-    credentials = "../gcs-backend.json"
-  }
-}
--- terraform/main.tf
+++ terraform/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_version = "~> 0.11.8"
+
+  backend "gcs" {
+    prefix = "terraform/state"
+    credentials = "../gcs-backend.json"
+  }
+}
+
 provider "digitalocean" {
   token = "${var.do_token}"
 }
