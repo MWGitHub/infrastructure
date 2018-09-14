ffe920a4e92ae2c36abb375718098ac00de2aede
Michael Wang <mwwpublic@gmail.com>
2018-09-14 16:48:17 -0400

    refactor: remove dead code

--- terraform/modules/kubernetes-gcp/variables.tf
+++ terraform/modules/kubernetes-gcp/variables.tf
@@ -2,9 +2,3 @@ variable project_id {
   type = "string"
   description = "Id of the GCP project."
 }
-
-variable "regions" {
-  type = "list"
-  description = "Regions for the clusters."
-  default = ["us-east1", "us-west1"]
-}
